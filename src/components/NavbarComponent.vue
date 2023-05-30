7b893ca4fab50edb6f5500e3467b0de183583a5a
Cymekk <cymerman944@gmail.com>
2023-05-30 12:46:54 +0200

    fixed some bugs

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,11 +1,13 @@
 <template>
 	<navbar class="navbar">
-		<div class="logo">
-			<i class="fa-regular fa-note-sticky"></i>
-			<h1>NoteApp</h1>
-		</div>
+		<div class="container">
+			<div class="logo">
+				<i class="fa-regular fa-note-sticky"></i>
+				<h1>NoteApp</h1>
+			</div>
 
-		<button v-if="storeAuth.user.id" @click="storeAuth.logoutUser">Logout {{ storeAuth.user.email }}</button>
+			<button v-if="storeAuth.user.id" @click="storeAuth.logoutUser">Logout {{ storeAuth.user.email }}</button>
+		</div>
 	</navbar>
 </template>
 <script setup>
@@ -19,47 +21,37 @@ const storeAuth = useStoreAuth()
 	background-color: rgb(0, 97, 29);
 	width: 100%;
 	height: 50px;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
 	padding: 1em;
 
-	.logo {
+	.container {
+		max-width: 1024px;
+		margin: 0 auto;
 		display: flex;
-		align-items: center;
-		i {
-			color: #fff;
-			font-size: 2rem;
+		justify-content: space-between;
+		.logo {
+			display: flex;
+			align-items: center;
+			i {
+				color: #fff;
+				font-size: 2rem;
+			}
+
+			h1 {
+				color: #fff;
+				font-size: 1.2rem;
+				margin-left: 0.25em;
+			}
+
+			button {
+				background: none;
+				border: none;
+				color: #fff;
+				padding: 0.5em;
+				border: 1px solid #fff;
+				border-radius: 5px;
+				cursor: pointer;
+			}
 		}
-
-		h1 {
-			color: #fff;
-			font-size: 1.2rem;
-			margin-left: 0.25em;
-		}
-	}
-
-	button{
-		background: none;
-		border: none;
-		color: #fff;
-		padding: 0.5em;
-		border: 1px solid #fff;
-		border-radius: 5px;
-		cursor: pointer;
-	}
-}
-
-.mobile-nav {
-	background-color: tomato;
-	position: absolute;
-	padding: 5px;
-	width: 100%;
-
-	a {
-		font-size: 1rem;
-		text-decoration: none;
-		color: #fff;
 	}
 }
 </style>
